11ea1b7831bd836bb55021356fb53b46363b7fb6
Omer Yariv <omer@knod.es>
2012-07-25 14:52:03 -0400

    copyright

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,3 +1,8 @@
 <?php
-
+/**
+ * li3_ensureindex: ensuring mongodb indexes
+ *
+ * @copyright     Copyright 2012, Knodes (http://knod.es)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
 ?>
--- tests/cases/extensions/command/EnsureIndexesTest.php
+++ tests/cases/extensions/command/EnsureIndexesTest.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * li3_ensureindex: ensuring mongodb indexes
+ *
+ * @copyright     Copyright 2012, Knodes (http://knod.es)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
 
 namespace li3_ensureindex\tests\cases\extensions\command;
 
