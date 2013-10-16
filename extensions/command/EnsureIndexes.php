efb536ae6f00165b6fa670d9c8f09e60da06d54c
Chris Jones <leeked@gmail.com>
2013-10-16 01:45:39 -0400

    Minor cleanup and fixing issue when MongoDb doesn't autoconnect

--- extensions/command/EnsureIndexes.php
+++ extensions/command/EnsureIndexes.php
@@ -9,86 +9,85 @@
 namespace li3_ensureindex\extensions\command;
 
 use Exception;
-use lithium\core\Environment;
 use lithium\core\Libraries;
-use lithium\analysis\Inspector;
 
-class EnsureIndexes extends \lithium\console\Command {
-
+class EnsureIndexes extends \lithium\console\Command
+{
     public $model = null;
 
     protected $defaults = array(
-        'unique' => false,
-        'dropDups' => false,
+        'unique'     => false,
+        'dropDups'   => false,
         'background' => true,
-        'safe' => true,
-        'timeout' => 10000
+        'safe'       => true,
+        'timeout'    => 10000,
     );
 
-    public function run() {
+    public function run()
+    {
         $models = Libraries::locate('models', $this->model);
 
-        if( empty( $models ) ) {
-            throw new Exception( "could not locate model " . $this->model );
+        if (empty($models)) {
+            throw new Exception('Could not locate model: ' . $this->model);
         }
 
-        if( !is_array( $models ) ) {
-            $models = array( $models );
+        if (!is_array($models)) {
+            $models = array($models);
         }
 
         $counters = array(
-            'models' => 0,
-            'indexes' => 0
+            'models'  => 0,
+            'indexes' => 0,
         );
 
-        foreach( $models as $model ) {
+        foreach ($models as $model) {
             $model = str_replace("\\\\", "\\", $model);
-
-            if( property_exists( $model, '_indexes' ) ) {
-                $db = $model::connection();
-
-                if ( !is_a( $db, 'lithium\data\source\MongoDb' ) ) {
-                    throw new Exception( "This command works only with MongoDB, sorry" );
-                }
-
-                $conn = $db->server->{$db->_config['database']};
-                $collection = $conn->{$model::meta('source')};
-               
+            if (property_exists($model, '_indexes')) {
                 $indexes = $model::$_indexes;
-                if( empty( $indexes ) ) {
+                if (empty($indexes)) {
                     continue;
                 }
-                $counters['models']++;
-                $this->out( "{:heading}ensuring indexes for model $model{:end}" );
 
-                foreach( $indexes as $name => $index ) {
-                    if( empty( $index['keys'] ) ) {
-                        $this->out( "skipping index $name as it didn't define any keys" );
+                $db = $model::connection();
+                if (!is_a($db, 'lithium\data\source\MongoDb')) {
+                    throw new Exception('This command works only with MongoDB');
+                }
+
+                $db->connect();
+                $collection = $db->connection->{$model::meta('source')};
+
+                $counters['models']++;
+                $this->out('{:heading}Ensuring indexes for model: ' . $model . '{:end}');
+
+                foreach ($indexes as $name => $index) {
+                    if (empty($index['keys'])) {
+                        $this->error(' * No keys defined for index: ' . $name);
                         continue;
                     }
 
                     $keys = $index['keys'];
-                    unset( $index['keys'] );
-                    if( !isset( $index['name'] ) ) {
+                    unset($index['keys']);
+
+                    if (!isset($index['name'])) {
                         $index['name'] = $name;
                     }
 
                     $options = $index + $this->defaults;
 
                     try {
-                       $collection->ensureIndex( $keys, $options ); 
+                        $collection->ensureIndex($keys, $options);
                     } catch (Exception $e) {
-                        $this->error( "{:command}$name{:end} failed with: " . $e->getMessage() );
+                        $this->error(' * Failed: {:command}' . $name . '{:end} with: ' . $e->getMessage());
+                        continue;
                     }
+
                     $counters['indexes']++;
-                    $this->out( "{:command}$name{:end} ensured" );
-                   
+                    $this->out(' * Ensured: {:command}' . $name . '{:end}');
                 }
-                
             }
         }
 
-        $this->header( "{:heading}done. ensured a total of {$counters['indexes']} indexes on {$counters['models']} models{:end}" );
+        $this->out('');
+        $this->header('{:heading}Done! Ensured a total of ' . $counters['indexes'] . ' indexes on ' . $counters['models'] . ' models{:end}');
     }
-
 }
